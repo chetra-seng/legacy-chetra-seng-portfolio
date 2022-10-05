aeadc9a0cb40d0df86c84aebcdb39674ef13c513
Chetra Seng <sengchetra.love@icloud.com>
2022-10-06 01:07:05 +0700

    Added Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,24 @@
 import React, { FC } from "react";
 import "./index.scss";
 import Sidebar from "../Sidebar";
+import { Outlet } from "react-router-dom";
 
-const index:FC = () => {
+const index: FC = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;html&gt;</span>
+        </span>
+      </div>
+    </div>
   );
 };
 
