9d1b2b2aa50535f8c30ebb62f1b957f1a5733efe
Chetra Seng <sengchetra.love@icloud.com>
2022-10-09 01:11:08 +0700

    Remove gsap

--- src/components/Home/Logo/index.tsx
+++ src/components/Home/Logo/index.tsx
@@ -1,43 +1,12 @@
-import gsap from "gsap-trial";
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import { useEffect, useRef } from "react";
 import Showcase from "../../../assets/images/showcase.png";
 import "./index.scss";
 import Blob from "../../../assets/images/blob.svg";
 
 const Logo = () => {
-  const bgRef = useRef<HTMLDivElement>(null);
-  const solidRef = useRef<HTMLImageElement>(null);
-  const blobRef = useRef<HTMLImageElement>(null);
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(blobRef.current, {
-        opacity: 0,
-        duration: 4
-      });
-    gsap.fromTo(
-      solidRef.current,
-      { opacity: 0 },
-      {
-        opacity: 1,
-        delay: 2,
-        duration: 1,
-      }
-    );
-  }, []);
-
   return (
-    <div className="logo-container" ref={bgRef}>
-      <img src={Blob} alt="Coding" className="blob-container" ref={blobRef}/>
-      <img src={Showcase} alt="Chetra seng" className="solid-logo" ref={solidRef} />
+    <div className="logo-container">
+      <img src={Blob} alt="Coding" className="blob-container" />
+      <img src={Showcase} alt="Chetra seng" className="solid-logo" />
     </div>
   );
 };
