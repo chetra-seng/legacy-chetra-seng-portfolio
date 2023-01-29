7148c1428cf50e3aa0f5dd0fe52aac3dab81c696
Chetra Seng <sengchetra.love@icloud.com>
2023-01-29 23:43:24 +0700

    Updated home using tailwind

--- src/components/Home/Logo/index.tsx
+++ src/components/Home/Logo/index.tsx
@@ -4,15 +4,17 @@ import Blob from "../../../assets/images/blob.svg";
 
 const Logo = () => {
   return (
-    <div
-      className="z-0 w-80 h-80 flex flex-col animate-fadeIn"
-      style={{ backgroundImage: `url(${Blob})` }}
-    >
-      <img
-        src={Showcase}
-        alt="Profile"
-        className="w-[55%] mx-auto self-center my-auto"
-      />
+    <div className="w-[50%] flex justify-center">
+      <div
+        className="z-0 w-80 h-80 flex flex-col animate-fadeIn"
+        style={{ backgroundImage: `url(${Blob})` }}
+      >
+        <img
+          src={Showcase}
+          alt="Profile"
+          className="w-[55%] mx-auto self-center my-auto"
+        />
+      </div>
     </div>
   );
 };
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -18,44 +18,53 @@ const Home: React.FC = () => {
   }, []);
 
   return (
-    <>
-      <div className="max-lg:flex-col container home-page flex flex-row justify-around items-center w-screen">
-        <div className="w-[50%] self-center">
-          <h1 className="text-blue-100 text-5xl m-0 font-coolvetica font-medium before:content-['<h1>'] before:font-la-belle-aurore before:text-blue-400 before:absolute before:-mt-[2.5rem] before:left-1 before:opacity-60 before:text-lg after:content-['</h1>'] after:font-la-belle-aurore after:text-blue-400 after:text-lg after:absolute after:mt-6 after:ml-6 after:opacity-60 animate-fadeInBackWards">
-            <span className={`${letterClass} _11`}>H</span>
-            <span className={`${letterClass} _12`}>i</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'</span>
-            <span className={`${letterClass} _15`}>m</span>
-            <img
-              className="inline-block ml-4 mr-1 mb-3 animate-rotateIn animation-delay-1400"
-              src={C}
-              alt={"Logo"}
-            />
-            <AnimatedLetter
-              letterClass={letterClass}
-              strArray={name.split("")}
-              idx={16}
-            />
-            <br />
-            <AnimatedLetter
-              letterClass={letterClass}
-              strArray={job.split("")}
-              idx={16 + name.length}
-            />
-          </h1>
-          <h2 className="text-gray-200 mt-4 font-normal font-sans-serif text-xs tracking-widest animate-fadeInBackWards">
-            Full-stack Developer / Smart Contract Developer / University Student
-          </h2>
-          <Link className="flat-button" to={"/contact"}>
-            CONTACT ME
-          </Link>
+    <div className="w-screen flex justify-center">
+      <div className="h-screen flex flex-col justify-center">
+        <div className="max-lg:flex-col max-w-7xl self-center home-page flex flex-row justify-between items-center gap-20">
+          <div className="w-[50%] self-center flex flex-col gap-10">
+            <div className="flex flex-col gap-5">
+              <h1 className="text-blue-100 text-5xl m-0 font-coolvetica font-medium before:content-['<h1>'] before:font-la-belle-aurore before:text-blue-400 before:absolute before:-mt-[2.5rem] before:left-1 before:opacity-60 before:text-lg after:content-['</h1>'] after:font-la-belle-aurore after:text-blue-400 after:text-lg after:absolute after:mt-6 after:ml-6 after:opacity-60 animate-fadeInBackWards">
+                <span className={`${letterClass} _11`}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <br />
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'</span>
+                <span className={`${letterClass} _15`}>m</span>
+                <img
+                  className="inline-block ml-4 mr-1 mb-3 animate-rotateIn animation-delay-1400"
+                  src={C}
+                  alt={"Logo"}
+                />
+                <AnimatedLetter
+                  letterClass={letterClass}
+                  strArray={name.split("")}
+                  idx={16}
+                />
+                <br />
+                <AnimatedLetter
+                  letterClass={letterClass}
+                  strArray={job.split("")}
+                  idx={16 + name.length}
+                />
+              </h1>
+              <h2 className="text-gray-200 font-normal font-sans-serif text-xs tracking-widest animate-fadeInBackWards">
+                Full-stack Developer / Smart Contract Developer / University
+                Student
+              </h2>
+            </div>
+            <Link
+              className="text-[#47B5FF] text-base font-normal space-x-1 py-3 px-5 no-underline border-[1px] border-solid border-[#47B5FF] hover:bg-[#47B5FF] hover:text-[#333] w-44 text-center"
+              // className="flat-button"
+              to={"/contact"}
+            >
+              CONTACT ME
+            </Link>
+          </div>
+          <Logo />
         </div>
-        <Logo />
       </div>
       <Loader type={"ball-scale-multiple"} active />
-    </>
+    </div>
   );
 };
 
