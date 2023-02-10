a08ab83e21abf05956fc3c3b23de86b5cd05a813
Chetra Seng <sengchetra.love@icloud.com>
2023-02-11 01:57:33 +0700

    Added animation to home page with framer motion

--- src/components/Home/Logo/index.tsx
+++ src/components/Home/Logo/index.tsx
@@ -1,20 +1,26 @@
 import Showcase from "../../../assets/images/showcase.png";
-import "./index.scss";
 import Blob from "../../../assets/images/blob.svg";
+import { motion } from "framer-motion";
 
 const Logo = () => {
   return (
     <div className="w-[50%] flex justify-center">
-      <div
+      <motion.div
         className="z-0 w-80 h-80 flex flex-col animate-fadeIn"
         style={{ backgroundImage: `url(${Blob})` }}
+        initial={{ scale: 0.2, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 1, delay: 2 }}
       >
-        <img
+        <motion.img
           src={Showcase}
           alt="Profile"
           className="w-[55%] mx-auto self-center my-auto"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 3 }}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,11 +4,18 @@ import { Link } from "react-router-dom";
 import AnimatedLetter from "../AnimatedLetter";
 import Logo from "./Logo";
 import Loader from "react-loaders";
+import { motion, MotionProps } from "framer-motion";
 
 const Home: React.FC = () => {
   const name = "hetra Seng,";
   const job = "developer";
   const [letterClass, setLetterClass] = useState("text-animate");
+
+  const pageDelay: MotionProps = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { duration: 0.5, delay: 1 },
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,18 +26,24 @@ const Home: React.FC = () => {
   return (
     <div className="w-screen flex justify-center">
       <div className="h-screen flex flex-col justify-center">
-        <div className="max-lg:flex-col max-w-7xl self-center home-page flex flex-row justify-between items-center gap-20">
+        <motion.div
+          className="max-lg:flex-col max-w-7xl self-center home-page flex flex-row justify-between items-center gap-20"
+          {...pageDelay}
+        >
           <div className="w-[50%] self-center flex flex-col gap-10">
             <div className="flex flex-col gap-5">
-              <h1 className="text-blue-100 text-5xl m-0 font-coolvetica font-medium before:content-['<h1>'] before:font-la-belle-aurore before:text-blue-400 before:absolute before:-mt-[2.5rem] before:left-1 before:opacity-60 before:text-lg after:content-['</h1>'] after:font-la-belle-aurore after:text-blue-400 after:text-lg after:absolute after:mt-6 after:ml-6 after:opacity-60 animate-fadeInBackWards">
+              <h1 className="text-blue-100 text-5xl m-0 font-coolvetica font-medium before:content-['<h1>'] before:font-la-belle-aurore before:text-blue-400 before:absolute before:-mt-[2.5rem] before:left-1 before:opacity-60 before:text-lg after:content-['</h1>'] after:font-la-belle-aurore after:text-blue-400 after:text-lg after:absolute after:mt-6 after:ml-6 after:opacity-60">
                 <span className={`${letterClass} _11`}>H</span>
                 <span className={`${letterClass} _12`}>i</span>
                 <br />
                 <span className={`${letterClass} _13`}>I</span>
                 <span className={`${letterClass} _14`}>'</span>
                 <span className={`${letterClass} _15`}>m</span>
-                <img
-                  className="inline-block ml-4 mr-1 mb-3 animate-rotateIn animation-delay-1400"
+                <motion.img
+                  initial={{ rotate: 180, opacity: 0.2 }}
+                  animate={{ rotate: 0, opacity: 1 }}
+                  transition={{ duration: 1.2, delay: 1.2 }}
+                  className="inline-block ml-4 mr-1 mb-3"
                   src={C}
                   alt={"Logo"}
                 />
@@ -46,21 +59,32 @@ const Home: React.FC = () => {
                   idx={16 + name.length}
                 />
               </h1>
-              <h2 className="text-gray-200 font-normal font-sans-serif text-xs tracking-widest animate-fadeInBackWards">
+              <motion.h2
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 1.2, delay: 2 }}
+                className="text-gray-200 font-normal font-sans-serif text-xs tracking-widest"
+              >
                 Full-stack Developer / Smart Contract Developer / University
                 Student
-              </h2>
+              </motion.h2>
             </div>
-            <Link
-              className="text-[#47B5FF] text-base font-normal space-x-1 py-3 px-5 no-underline border-[1px] border-solid border-[#47B5FF] hover:bg-[#47B5FF] hover:text-[#333] w-44 text-center"
-              // className="flat-button"
-              to={"/contact"}
+            <motion.div
+              initial={{ y: 200, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1.2, delay: 2.3 }}
             >
-              CONTACT ME
-            </Link>
+              <Link
+                className="text-[#47B5FF] text-base font-normal space-x-1 py-3 px-5 no-underline border-[1px] border-solid border-[#47B5FF] hover:bg-[#47B5FF] hover:text-[#333] w-44 text-center"
+                // className="flat-button"
+                to={"/contact"}
+              >
+                CONTACT ME
+              </Link>
+            </motion.div>
           </div>
           <Logo />
-        </div>
+        </motion.div>
       </div>
       <Loader type={"ball-scale-multiple"} active />
     </div>
