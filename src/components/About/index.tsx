aa30e55fcb0e8303222321c28ccb14fb53810cfd
Chetra Seng <sengchetra.love@icloud.com>
2023-02-11 10:16:40 +0700

    Added animation to about page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,9 +8,16 @@ import NextIcon from "../../assets/images/nextjs.png";
 import FlaskIcon from "../../assets/images/flask.png";
 import SolidityIcon from "../../assets/images/solidity.png";
 import GitIcon from "../../assets/images/git.png";
+import { motion, MotionProps } from "framer-motion";
 
 const About = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
+
+  const pageDelay: MotionProps = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { duration: 0.5, delay: 1 },
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -20,7 +27,10 @@ const About = () => {
   return (
     <div className="w-screen flex justify-center">
       <div className="h-screen flex flex-col justify-center">
-        <div className="max-lg:flex-col max-w-5xl self-center home-page flex flex-row justify-between items-center">
+        <motion.div
+          className="max-lg:flex-col max-w-5xl self-center home-page flex flex-row justify-between items-center"
+          {...pageDelay}
+        >
           <div className="w-[50%] flex flex-col gap-10">
             <h1 className="text-blue-400 text-5xl m-0 font-coolvetica font-medium before:content-['<h1>'] before:font-la-belle-aurore before:text-blue-400 before:absolute before:-mt-[2.5rem] before:left-1 before:opacity-60 before:text-lg after:content-['</h1>'] after:font-la-belle-aurore after:text-blue-400 after:text-lg after:absolute after:mt-6 after:ml-6 after:opacity-60 animate-fadeInBackWards">
               <AnimatedLetter
@@ -30,29 +40,46 @@ const About = () => {
               />
             </h1>
             <div className="text-sm text-blue-100 font-sans-serif font-light flex flex-col gap-3">
-              <p>
+              <motion.p
+                initial={{ opacity: 0, y: 200, scaleY: 0.5 }}
+                animate={{ opacity: 1, y: 0, scaleY: 1 }}
+                transition={{ duration: 1, delay: 1.2 }}
+              >
                 Currently a full-stack developer at Techo Startup Center, and a
                 fourth year university student at Royal University of Phnom
                 Penh. A seeker who strives for opportunities to work and
                 participate in innovative projects using the latest technologies
                 available.
-              </p>
-              <p>
+              </motion.p>
+              <motion.p
+                initial={{ opacity: 0, y: 200, scaleY: 0.5 }}
+                animate={{ opacity: 1, y: 0, scaleY: 1 }}
+                transition={{ duration: 1, delay: 1.5 }}
+              >
                 Experienced working with different languages such as Python,
                 Java, Javascript, Typescript and Solidity. Have used front-end
                 and backend framework such as Spring, Flask, React and NextJs.
                 Able to work with Docker, setup CI/CD pipeline with Jenkins,
                 despite not being an expert.
-              </p>
-              <p>
+              </motion.p>
+              <motion.p
+                initial={{ opacity: 0, y: 200, scaleY: 0.5 }}
+                animate={{ opacity: 1, y: 0, scaleY: 1 }}
+                transition={{ duration: 1, delay: 1.8 }}
+              >
                 Dream of becoming a software engineer by building up skills,
                 following the best practice and design patterns. Interested in
                 new concepts and technologies as well as actively looking for
                 opportunities to colaborate in opensource projects.
-              </p>
+              </motion.p>
             </div>
           </div>
-          <div className="w-[50%] h-96 flex flex-col justify-center mb-36">
+          <motion.div
+            className="w-[50%] h-96 flex flex-col justify-center mb-36"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 2, duration: 1 }}
+          >
             <div className="cubespinner">
               <div className="face1">
                 <img src={SpringIcon} alt="Spring Logo" />
@@ -73,8 +100,8 @@ const About = () => {
                 <img src={GitIcon} alt="Git Logo" />
               </div>
             </div>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
       <Loader type={"ball-scale-multiple"} active />
     </div>
