e7f37e8998ddd899b5a761e40924cd05c157419b
Chetra Seng <sengchetra.love@icloud.com>
2023-02-11 23:18:08 +0700

    Added animation to contact page

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -4,9 +4,16 @@ import AnimatedLetter from "../AnimatedLetter";
 import "./index.scss";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { FiFacebook, FiGithub, FiLinkedin } from "react-icons/fi";
+import { motion, MotionProps } from "framer-motion";
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
+
+  const pageDelay: MotionProps = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { duration: 0.5, delay: 1 },
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,7 +22,10 @@ const Contact = () => {
   }, []);
   return (
     <div className="w-screen flex justify-center">
-      <div className="h-screen flex flex-col justify-center">
+      <motion.div
+        className="h-screen flex flex-col justify-center"
+        {...pageDelay}
+      >
         <div className="max-lg:flex-col max-w-5 self-center about-page flex flex-row justify-between items-center">
           <div className="w-[50%] flex flex-col gap-10">
             <h1 className="text-blue-400 text-5xl m-0 font-coolvetica font-medium before:content-['<h1>'] before:font-la-belle-aurore before:text-blue-400 before:absolute before:-mt-[2.5rem] before:left-1 before:opacity-60 before:text-lg after:content-['</h1>'] after:font-la-belle-aurore after:text-blue-400 after:text-lg after:absolute after:mt-6 after:ml-6 after:opacity-60 animate-fadeInBackWards">
@@ -25,7 +35,12 @@ const Contact = () => {
                 idx={15}
               />
             </h1>
-            <div className="text-sm text-blue-100 font-sans-serif font-light flex flex-col gap-3">
+            <motion.div
+              className="text-sm text-blue-100 font-sans-serif font-light flex flex-col gap-3"
+              initial={{ opacity: 0, scaleX: 0.5 }}
+              animate={{ opacity: 1, scaleX: 1 }}
+              transition={{ delay: 1.2, duration: 1 }}
+            >
               <p>
                 Leave a message if you're interested in helping me achieve my
                 dream. If you leave near by, come and have a cup of coffee
@@ -34,37 +49,52 @@ const Contact = () => {
               <p>
                 In case you still prefer to contact me via phone or social
                 media, I'll leave my contacts here
-                <ul className="list-inside font-semibold p-5 flex flex-col gap-3">
-                  <li>
-                    <a
+                <ul className="list-inside font-semibold p-5 flex flex-col gap-3 items-start">
+                  <motion.li
+                    initial={{ y: 200, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1, delay: 2 }}
+                  >
+                    <motion.a
                       href="https://github.com/chetra-seng"
                       className="flex flex-row items-center gap-2 hover:text-blue-400"
+                      whileHover={{ scale: 1.1 }}
                     >
                       <FiGithub size={18} />
-                      GitHub
-                    </a>
-                  </li>
-                  <li>
-                    <a
+                      GitHub: Chetra Seng
+                    </motion.a>
+                  </motion.li>
+                  <motion.li
+                    initial={{ y: 200, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1, delay: 2.5 }}
+                  >
+                    <motion.a
                       href="https://www.linkedin.com/in/chetra-s-5b4048ab/"
                       className="flex flex-row items-center gap-2 hover:text-blue-400"
+                      whileHover={{ scale: 1.1 }}
                     >
                       <FiLinkedin size={18} />
-                      LinkedIn
-                    </a>
-                  </li>
-                  <li>
-                    <a
+                      LinkedIn: Chetra Seng
+                    </motion.a>
+                  </motion.li>
+                  <motion.li
+                    initial={{ y: 200, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1, delay: 3 }}
+                  >
+                    <motion.a
                       href="https://web.facebook.com/chetraseng11"
                       className="flex flex-row items-center gap-2 hover:text-blue-400"
+                      whileHover={{ scale: 1.1 }}
                     >
                       <FiFacebook size={18} />
-                      Facebook
-                    </a>
-                  </li>
+                      Facebook: Chetra Seng
+                    </motion.a>
+                  </motion.li>
                 </ul>
               </p>
-            </div>
+            </motion.div>
           </div>
           <div className="bg-[#256D85] w-[45%] h-[20rem]">
             <MapContainer center={[11.555287, 104.915993]} zoom={13}>
@@ -77,7 +107,7 @@ const Contact = () => {
             </MapContainer>
           </div>
         </div>
-      </div>
+      </motion.div>
       <Loader type={"ball-scale-multiple"} active />
     </div>
   );
